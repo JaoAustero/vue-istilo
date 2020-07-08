c57a3d989ea12fe88f37e0e29c90f6993746b8f7
Jerald Austero <jerald.austero0229@gmail.com>
2020-07-08 16:05:26 +0800

    🔧 Vuepress Sidebar
    We added a test links for sidebar of the docs.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,37 +4,37 @@ module.exports = {
      *
      * @type {String}
      * @default ['/']
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#base}
      */
     base: '/',
 
     /**
      * VuePress uses cache-loader by default to greatly speed up the compilation of webpack.
-     * 
+     *
      * @type {Boolean|String}
      * @default [true]
-     * 
-     * @see {@link https://vuepress.vuejs.org/config/#cache} 
+     *
+     * @see {@link https://vuepress.vuejs.org/config/#cache}
      */
     cache: true,
-    
+
     /**
      * Description for the site.
-     * 
+     *
      * @type {String}
      * @default [undefined]
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#description}
      */
     description: 'A lightweight Vue component framework for website interface.',
 
     /**
      * Specify the output directory for vuepress build.
-     * 
+     *
      * @type {String},
      * @default ['.vuepress/dist']
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#dest}
      */
     dest: '.vuepress/dist',
@@ -44,7 +44,7 @@ module.exports = {
      *
      * @type {Array}
      * @default []
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#extrawatchfiles}
      */
     extraWatchFiles: [],
@@ -54,27 +54,27 @@ module.exports = {
      *
      * @type {Array}
      * @default [[]]
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#head}
      */
     head: [],
 
     /**
      * Specify the host to use for the dev server.
-     * 
+     *
      * @type {String}
      * @default ['0.0.0.0']
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#host}
      */
     host: 'docs.vue-istilo.loc',
 
     /**
      * Specify locales for i18n support. For more details, see the guide on Internationalization.
-     * 
+     *
      * @type {[path: string]: Object}
      * @default [undefined]
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#locales}
      */
     locales: undefined,
@@ -90,43 +90,71 @@ module.exports = {
 
     /**
      * Specify the port to use for the dev server.
-     * 
+     *
      * @type {Number}
      * @default [8080]
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#port}
      */
     port: 81,
 
     /**
      * A function to control what files should have <link rel="prefetch"> resource hints generated
-     * 
+     *
      * @type {Function}
      * @default [()=>true]
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#shouldprefetch}
      */
     shouldPrefetch: () => true,
 
     /**
      * Specify the temporary directory for client.
-     * 
+     *
      * @type {String}
      * @default ['/path/to/@vuepress/core/.temp']
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#temp}
      */
     temp: '/path/to/@vuepress/core/.temp',
 
     /**
      * Title for the site.
-     * 
+     *
      * @type {String}
      * @default ['undefined']
-     * 
+     *
      * @see {@link https://vuepress.vuejs.org/config/#title}
      */
     title: 'Vue Istilo CSS',
+
+    /**
+     * Theme
+     */
+    themeConfig:
+    {
+        activeHeaderLinks: true,
+        displayAllHeaders: true,
+        sidebar:
+        [
+            '/',
+            {
+                title: 'Getting Started',
+                collapsable: false,
+                children:
+                [
+                    {
+                        title: 'Installation',
+                        path: '/getting-started/installation',
+                    },
+                    {
+                        title: 'Release',
+                        path: '/getting-started/release',
+                    },
+                ],
+            },
+        ],
+    },
 
     /**
      * Markdown
@@ -135,7 +163,7 @@ module.exports = {
     {
         /**
          * Whether to show line numbers to the left of each code blocks.
-         * 
+         *
          * @type {Boolean}
          * @default [undefined]
          *
